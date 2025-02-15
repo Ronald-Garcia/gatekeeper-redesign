56520a101da2a848e6bd5f825438c74495c5686c
Ronald Garcia <rgarcia0303757@gmail.com>
2025-02-15 16:32:07 -0500

    Added demo connection for interlock and machine-api

--- web-interlock/src/data/api.ts
+++ web-interlock/src/data/api.ts
@@ -1,4 +1,5 @@
-import { API_MACHINE_URL } from "@/env"
+import { API_DB_URL, API_MACHINE_URL } from "@/env"
+import { UserType } from "./types";
 
 export const turnOnMachine = async (): Promise<boolean> => {
     const response = await fetch(
@@ -16,3 +17,44 @@ export const turnOnMachine = async (): Promise<boolean> => {
 
     return message.startsWith('s');
 }
+
+export const sendToMachine = async (message: string, data: UserType[]) => {
+    const response = await fetch(
+        `${API_MACHINE_URL}/demo`,
+        {
+            credentials: "include",
+            method: "POST",
+            body: JSON.stringify({
+                message: message,
+                data: data
+            })
+        }
+    );
+
+    const { m }: { m: string} = await response.json();
+    if (!response.ok) {
+        throw new Error(m);
+    }
+
+    return m;
+}
+// DEMO HELLO WORLD
+export const getAllUsers = async (): Promise<{ message: string, data: UserType[]}> => {
+
+    const response = await fetch(
+        `${API_DB_URL}/users`,
+        {
+            credentials: "include"
+        }
+    )
+
+    if (!response.ok) {
+        const { message }: { message: string } = await response.json();
+
+        throw new Error(message);
+    }
+
+    const { message, data }: {message: string, data: UserType[] }  = await response.json();
+
+    return { message, data};
+} 
