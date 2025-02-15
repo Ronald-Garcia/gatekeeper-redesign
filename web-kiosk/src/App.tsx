38c2e4121d3e48cda901ac92b13035b3e153da63
omunoz03 <omunoz4@jh.edu>
2025-02-15 17:46:28 -0500

    front

--- web-kiosk/src/App.tsx
+++ web-kiosk/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { sendHello } from './data/api'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => sendHello("HI2","hi@gmail.com")}>
           count is {count}
         </button>
         <p>
