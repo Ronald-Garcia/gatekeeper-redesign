6d6af5821cc34d08edd244908c9ab66fa2e4de76
ckolisko <koliskocameron@gmail.com>
2025-02-17 20:24:28 -0500

    Set up environment varaiables in api

--- api/src/db.ts
+++ api/src/db.ts
@@ -2,9 +2,14 @@
 import { pgTable, serial, varchar, text } from 'drizzle-orm/pg-core';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg'
+import "dotenv/config";
+
+//Make sure to make a env file for these variables.
+const PASSWORD = process.env.PASSWORD;
+const SERVERPORT = process.env.SERVERPORT;
 
 const pool =  new pg.Pool({
-  connectionString: `postgres://postgres:IntelCorei7@localhost:5432/team10_db`,
+  connectionString: `postgres://postgres:${PASSWORD}@localhost:${SERVERPORT}/team10_db`,
 });
 export const db = drizzle({ client: pool });
 const result = await db.execute('select 1');
