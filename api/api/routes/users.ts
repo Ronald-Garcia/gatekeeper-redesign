bfae3d4a10cde8c260eeb6a5ae6d66146cdba75c
ckolisko <koliskocameron@gmail.com>
2025-02-22 10:17:24 -0500

    switch case for sorts users

--- api/api/routes/users.ts
+++ api/api/routes/users.ts
@@ -1,7 +1,7 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { queryParamsSchema } from "../validators/schemas";
-import { like, SQL, or } from "drizzle-orm";
+import { queryUsersParamsSchema } from "../validators/schemas";
+import { like, SQL, or, desc, asc } from "drizzle-orm";
 import { usersTable } from "../db/schema";
 
 
@@ -10,7 +10,7 @@ export const userRoutes = new Hono();
 
 
 
-userRoutes.get("./users", zValidator("param", queryParamsSchema), async (c) => {
+userRoutes.get("./users", zValidator("param", queryUsersParamsSchema), async (c) => {
     const { page = 1, limit = 20, sort, search } = c.req.valid("param");
 
     const whereClause: (SQL | undefined)[] = [];
@@ -19,5 +19,28 @@ userRoutes.get("./users", zValidator("param", queryParamsSchema), async (c) => {
         whereClause.push(or(like(usersTable.name, search), like(usersTable.JHED, search)));
     }
 
-    
+    const orderByClause: SQL[] = [];
+
+    switch (sort){
+        case "name_desc" :
+            orderByClause.push(desc(usersTable.name));
+            break;
+        case "name_asc" :
+            orderByClause.push(asc(usersTable.name));
+            break;
+        case "year_asc" :
+            orderByClause.push(desc(usersTable.graduationYear));
+            break;
+        case "year_desc" :
+            orderByClause.push(asc(usersTable.graduationYear));
+            break;
+        case "jhed_asc" :
+            orderByClause.push(desc(usersTable.JHED));
+            break;
+        case "jhed_desc" :
+            orderByClause.push(asc(usersTable.JHED));
+            break;
+
+    }
+
 })
