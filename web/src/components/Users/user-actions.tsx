a2fddcd5c68a558189a4c04526494068b2debecf
omunoz03 <omunoz4@jh.edu>
2025-02-21 23:41:13 -0500

    users componenet

--- web/src/components/Users/user-actions.tsx
+++ web/src/components/Users/user-actions.tsx
@@ -1,4 +1,37 @@
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+  } from "@/components/ui/dropdown-menu";
+
 export default function user-actions{
+
+    type userActionsProps {
+        // user 
+     isActive : (active: boolean) => void;
+    };
+
+
+    return (
+        <>
+       <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="absolute top-2 right-2 deck-actions"
+          >
+            ...
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={handleAddTraining}>Add Training</DropdownMenuItem>
+          <DropdownMenuItem onClick={handleDelete} className="delete-text-red">Delete</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+        </>
+    )
+
 
 
 
--- web/src/components/Users/users.tsx
+++ web/src/components/Users/users.tsx
@@ -1,7 +1,22 @@
+import { useStore } from "@nanostores/react";
+import { users } from "@/data/store";
+import UserComponent from "./user";
 
+export default function UsersComponent(){
+    const userList = useStore(users);
 
-export default function users(){
-
+    return (
+        <div>
+            <h1>Users:</h1>
+                {userList.length ===0 ? (
+                    <p> No users</p>
+                ):(
+                        userList.map((user) => ( 
+                            <UserComponent key={user.getCardNumber()}user={user}/> 
+                        ))
+                )}
+        </div>
+    );
 
 
 }
