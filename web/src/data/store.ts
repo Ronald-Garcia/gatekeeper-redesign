9269854110364f30dbb1319dc9c0df66ad0cd08e
omunoz03 <omunoz4@jh.edu>
2025-02-21 23:49:21 -0500

    users componenet 2

---
+++ web/src/data/store.ts
@@ -0,0 +1,20 @@
+import { User } from "@/components/components/types/user";
+import { atom } from "nanostores";
+
+
+export const users = atom<User[]> ([]);
+
+export function addUser(user:User) {
+    users.set([...users.get(), user]);
+}
+
+export function deleteUser(cardNum:number) {
+    users.set(users.get().filter((user:User) => user.getCardNumber() !== cardNum));
+}
+
+
+
+
+
+
+
