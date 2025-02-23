2ae35cd2a88bffaf3f2575aa1dcf1232dce710bf
omunoz03 <omunoz4@jh.edu>
2025-02-23 18:30:04 -0500

    mutation budget codes

--- web/src/data/store.ts
+++ web/src/data/store.ts
@@ -1,35 +1,43 @@
-import { User } from "@/components/components/types/user"
+import { User } from "@/components/components/types/user";
 import { atom } from "nanostores";
-import { budgetCode } from "@/components/components/types/budgetCode";
+import { BudgetCode } from "@/components/components/types/budgetCode";
 
+export const $users = atom<User[]>([]);
+export const $codes = atom<BudgetCode[]>([]);
 
-export const $users = atom<User[]> ([]);
-export const $codes = atom<budgetCode[]> ([]);
-
-export function addUser(user:User) {
-    $users.set([...$users.get(), user]);
+export function addUser(user: User) {
+  $users.set([...$users.get(), user]);
 }
 
-export function deleteUserById(id :number) {
-    $users.set($users.get().filter((user:User) => user.getId() !== id));
+export function deleteUserById(id: number) {
+  $users.set($users.get().filter((user: User) => user.getId() !== id));
+}
 
-
-export function addTraining(id :number) {
-    $users.set($users.get().filter((user:User) => user.getId() !== id));
+export function addTraining(id: number) {
+  $users.set($users.get().filter((user: User) => user.getId() !== id));
 }
 
 export function setUsers(userList: User[]) {
-    $users.set(userList);
+  $users.set(userList);
 }
 
-export function setBudgetCodes(codeList:budgetCode[]){
-    $codes.set(codeList);
+export function banUserFlag(userId: number) {}
+
+//logic to update user
+export function updateUserById(userId: number) {
+  return User;
 }
 
-export function addBudgetCode(code:budgetCode) {
-    codes.set([...$codes.get(), code]);
+export function setBudgetCodes(codeList: BudgetCode[]) {
+  $codes.set(codeList);
 }
 
-export function deleteBudgetCode(codeNum:string) {
-    $codes.set($codes.get().filter((code:budgetCode) => code.getCode() !== codeNum));
+export function addBudgetCode(code: BudgetCode) {
+  $codes.set([...$codes.get(), code]);
+}
+
+export function deleteBudgetCodeByNum(codeNum: number) {
+  $codes.set(
+    $codes.get().filter((code: BudgetCode) => code.getCode() !== codeNum),
+  );
 }
---
+++ web/src/hooks/budgetCodes-mutation-hook.tsx
@@ -0,0 +1,60 @@
+import { toast } from "sonner";
+import { BudgetCode } from "../components/components/types/budgetCode";
+import { createBudgetCode, 
+    deleteBudgetCode,
+     editBudgetCode } from "../data/api";
+import { addBudgetCode, deleteBudgetCodeByNum, $codes } from "../data/store";
+
+function useMutationBudgetCodes() {
+
+  const addNewBudgetCode = async (budget: BudgetCode) => {
+    try {
+      const { data } = await createBudgetCode(budget);
+      addBudgetCode(data);
+      return data;
+    } catch (e) {
+      const errorMessage = (e as Error).message;
+      toast.error("Sorry! There was an error adding a budget code 🙁", {
+        description: errorMessage,
+      });
+    }
+  };
+
+ 
+  const removeBudgetCode = async (codeId: number) => {
+    try {
+      await deleteBudgetCode(codeId);
+      deleteBudgetCodeByNum(codeId);
+    } catch (e) {
+      const errorMessage = (e as Error).message;
+      toast.error("Sorry! There was an error removing the budget code 🙁", {
+        description: errorMessage,
+      });
+    }
+  };
+
+  
+  const updateBudgetCode = async (budget: BudgetCode) => {
+    try {
+      const { data } = await editBudgetCode(budget);
+      const updatedCodes = $codes.get().map((code: BudgetCode) =>
+        code.getCode() === data.getCode() ? data : code
+      );
+      $codes.set(updatedCodes);
+      return data;
+    } catch (e) {
+      const errorMessage = (e as Error).message;
+      toast.error("Sorry! There was an error updating the budget code 🙁", {
+        description: errorMessage,
+      });
+    }
+  };
+
+  return {
+    addNewBudgetCode,
+    removeBudgetCode,
+    updateBudgetCode,
+  };
+}
+
+export default useMutationBudgetCodes;
