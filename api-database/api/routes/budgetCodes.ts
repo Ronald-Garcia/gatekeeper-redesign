2f09a904cb008c3c8b1ba58588d3f5a5a48afaab
A-Archer <adrian.archer2004@hotmail.com>
2025-02-22 20:44:24 -0500

    Added deletion for budget code

--- api-database/api/routes/budgetCodes.ts
+++ api-database/api/routes/budgetCodes.ts
@@ -1,9 +1,11 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { queryBudgetCodesParamsSchema, createBudgetCode, deleteBudgetCode } from "../validators/schemas";
-import { like, SQL, or, desc, asc } from "drizzle-orm";
+import { like, SQL, or, desc, asc, eq } from "drizzle-orm";
 import { budgetCodes } from "../db/schema";
 import { db } from "../db/index";
+import { HTTPException} from "hono/http-exception";
+
 
 
 export const budgetCodesRoutes = new Hono();
@@ -23,12 +25,26 @@ budgetCodesRoutes.post("/budgetCodes", zValidator("json", createBudgetCode), asy
             name,
             budgetCode
         })
-        .returning()
+        .returning();
 
     return c.json(newBudgetCode, 201);
 
 })
 
 budgetCodesRoutes.delete("/budgetCodes", zValidator("json", deleteBudgetCode), async (c)=>{
+    const { id } = c.req.valid("json");
+
+    const budgetCode = await db.select().from(budgetCodes).where(eq(budgetCodes.id, id))
+
+    if (!budgetCode) {
+        throw new HTTPException(404, { message: "Budget Code not found!"});
+    }
+
+    const deletedBudgetCode = await db
+        .delete(budgetCodes)
+        .where(eq(budgetCodes.id, id))
+        .returning();
+
+    return c.json(deletedBudgetCode, 200)
 
 })
