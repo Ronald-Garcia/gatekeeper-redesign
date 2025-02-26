0ca38a829c4965f73e60fbae9de47e3c2de346d7
ckolisko <koliskocameron@gmail.com>
2025-02-26 16:05:41 -0500

    chema change

--- api/api/db/schema.ts
+++ api/api/db/schema.ts
@@ -1,48 +1,61 @@
 
-import { integer, pgTable, serial, text, boolean, date } from 'drizzle-orm/pg-core';
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable('users_table', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  cardNum: text('card_number').notNull().unique(),
-  lastDigitOfCardNum: integer('last_digit_of_card_num').notNull(),
-  JHED: text('JHED').notNull(),
-  graduationYear: date('graduation_year'),
-  isAdmin: boolean('is_admin')
+export const users = pgTable("users_table", {
+  id: serial().primaryKey(),
+  name: text().notNull(),
+  cardNum: text().notNull().unique(),
+  lastDigitOfCardNum: integer().notNull(),
+  JHED: text().notNull(),
+  isAdmin: integer().notNull(),
+  graduationYear: integer() //This is optional because some users like Rich do not have grad year.
 });
 
-//Table that acts as the "trainings" for a user
-export const userMachineRelation = pgTable('user_machine_table',{
-  id: serial('id').primaryKey(),
-  userId: integer('user_id').notNull().references(() => usersTable.id, {onDelete: 'cascade'}),
-  machineId: serial('machine_id').notNull().references(()=> machinesTable.id, {onDelete:'cascade'})
+export const machineTypes = pgTable("machine_type" , {
+  type: text().primaryKey().notNull()
+})
+
+export const machines = pgTable("machines_table", {
+  id: serial().primaryKey(),
+  hourlyRate: integer().notNull(),
+  name: text().notNull(),
+  machineType: text().notNull().references(() => machineTypes.type, {onDelete:"cascade"})
+})
+
+//Table that acts as the "trainings" completed for a user
+export const userMachineType = pgTable("user_machine_type",{
+  id: serial().primaryKey(),
+  userId: serial().notNull().references(() => users.id, {onDelete: "cascade"}),
+  machineType: text().notNull().references(()=> machineTypes.type, {onDelete:"cascade"})
 });
 
-export const machinesTable = pgTable('machines_table', {
-  id: serial('id').primaryKey(),
-  hourlyRate: integer('hourly_rate').notNull(),
-  name: text("name").notNull(),
-  machineType: serial("machine_type_id").notNull().references(() => machineTypes.id, {onDelete:'cascade'})
+export const budgetCodes = pgTable("budgetCodes" , {
+  id: serial().primaryKey(),
+  budgetCode: text().notNull(),
+  name: text().notNull(),
 })
 
-export const machineTypes = pgTable('machine_type' , {
-  id: serial('id').primaryKey(),
-  type: text('type').notNull()
-})
+export const userBudgetCodeTable = pgTable("user_budget_code_table",{
+  id: serial().primaryKey(),
+  userId: serial().notNull().references(() => users.id, {onDelete: "cascade"}),
+  budgetCodeId: serial().notNull().references(()=> machines.id, {onDelete:"cascade"})
+});
+
+export type InsertUser = typeof users.$inferInsert;
+export type SelectUser = typeof users.$inferSelect;
 
 
-export type InsertUser = typeof usersTable.$inferInsert;
-export type SelectUser = typeof usersTable.$inferSelect;
-
-
-export type InsertMachine = typeof machinesTable.$inferInsert;
-export type SelectMachine = typeof machinesTable.$inferSelect;
+export type InsertMachine = typeof machines.$inferInsert;
+export type SelectMachine = typeof machines.$inferSelect;
 
 export type InsertMachineType = typeof machineTypes.$inferInsert;
 export type SelectMachineType = typeof machineTypes.$inferSelect;
 
-export type InsertUserMachineRelation = typeof userMachineRelation.$inferInsert;
-export type SelectUserMachineRelation = typeof userMachineRelation.$inferSelect;
+export type InsertUserMachineRelation = typeof userMachineType.$inferInsert;
+export type SelectUserMachineRelation = typeof userMachineType.$inferSelect;
+
+export type InsertBudgetCode = typeof budgetCodes.$inferInsert;
+export type SelectBudgetCode = typeof budgetCodes.$inferSelect
 
 
 
