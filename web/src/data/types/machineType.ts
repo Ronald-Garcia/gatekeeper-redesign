69c7fc224c6bcd384313221cbfc06831b5cc71ba
omunoz03 <omunoz4@jh.edu>
2025-02-26 15:54:47 -0500

    user dialogue

--- web/src/data/types/machineType.ts
+++ web/src/data/types/machineType.ts
@@ -1,6 +1,21 @@
 export class MachineType {
+    private id: number;
+    private name: string;
 
+    constructor(
+        id: number,
+        name: string
+    ){
+        this.id = id;
+        this.name = name;
+    }
 
+    getId(): number {
+        return this.id;
+    }
 
-    
+    getName(): string {
+        return this.name;
+    }
+
 }
