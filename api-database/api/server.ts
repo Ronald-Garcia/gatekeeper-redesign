f2e97e7e7d4c655f2165b3b6284d50e454a9980f
ckolisko <koliskocameron@gmail.com>
2025-02-26 22:23:15 -0500

    Removed the .ts from app

--- api-database/api/server.ts
+++ api-database/api/server.ts
@@ -1,5 +1,5 @@
 import { serve } from "@hono/node-server";
-import app from "./app.ts";
+import app from "./app";
 
 const port = Number(process.env.PORT) || 3000;
 console.log(`Server is running on port ${port}`);
