a2f307a63bb2368f9312b471bbc6bd215fc45271
ckolisko <koliskocameron@gmail.com>
2025-03-11 23:44:03 -0400

    cyp: attempt with no environment variables/ hard code

--- web/src/env.ts
+++ web/src/env.ts
@@ -1,4 +1,6 @@
-export const API_DB_URL: string = import.meta.env.VITE_API_DB_URL;
+export const API_DB_URL: string = "http://localhost:3000";
+//export const API_DB_URL: string = import.meta.env.VITE_API_DB_URL;
+
 export const API_MACHINE_URL: string = import.meta.env.VITE_API_MACHINE_URL;
 export const BASE_URL = import.meta.env.BASE_URL || "/";
   
