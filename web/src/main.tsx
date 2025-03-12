eb7289c8f8e9a02755800dfeb217b909390ba2bc
ckolisko <koliskocameron@gmail.com>
2025-03-12 12:56:55 -0400

    refactored assets to a public folder, added a icon, done to make deployment work

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,11 +6,6 @@ import App from "./App";
 createRoot(document.getElementById("root")!).render(
   
   <StrictMode>
-    <head>
-      <link rel="stylesheet" href="https://use.typekit.net/vxy8tas.css"></link>
-      <link rel="stylesheet" href="https://use.typekit.net/jge8wgr.css"></link>
-      <link rel="stylesheet" href="https://use.typekit.net/lnm5qra.css"></link>
-    </head>
     <App />
   </StrictMode>,
 );
