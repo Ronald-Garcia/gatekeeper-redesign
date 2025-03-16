4f868697f6ef55a3f6b33b753fe56ccdbf928376
Ronald Garcia <rgarcia0303757@gmail.com>
2025-03-16 14:25:19 -0400

    Added email feature

--- api-database/api-files/emails/email-template.tsx
+++ api-database/api-files/emails/email-template.tsx
@@ -1,12 +1,39 @@
+import * as React from "react"
 
 interface EmailTemplateProps {
-  firstName: string;
+  data: FinancialStatementType[];
+}
+
+
+interface FinancialStatementType {
+  id: number,
+  userId: number,
+  budgetCode: number,
+  machineId: number,
+  startTime: number,
+  endTime: number,
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
-  firstName: string,
-}) => (
-  <div>
-    <h1>Welcome, {firstName}!</h1>
-  </div>
-);
+  data
+}: EmailTemplateProps) => {
+  return (
+    <div>
+      {data.map(d => {
+        return (<div>
+          {d.id}
+
+          {d.userId}
+          
+          {d.machineId}
+
+          {d.budgetCode}
+          
+          {d.startTime}
+
+          {d.endTime}
+        </div>)
+      })}
+    </div>
+  )
+}
