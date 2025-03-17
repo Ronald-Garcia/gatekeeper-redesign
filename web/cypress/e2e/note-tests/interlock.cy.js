daaadba7f09f409da073a88cea3a4067143c97d2
omunoz03 <omunoz4@jh.edu>
2025-03-16 22:51:44 -0400

    current tests

---
+++ web/cypress/e2e/note-tests/interlock.cy.js
@@ -0,0 +1,59 @@
+/// <reference types="cypress" />
+
+const WEB_URL = "http://localhost:5173";
+
+// User with machine access for LATHE 5000
+const userWithAccessCard = "0123456789111111";
+// User without machine access
+const userWithoutAccessCard = "0123456789222222";
+
+describe("Interlock Page E2E", () => {
+  beforeEach(() => {
+
+    cy.visit(`${WEB_URL}/interlock`);
+  });
+
+  context("User with machine access", () => {
+    it("allows the user to select a budget code and start the machine", () => {
+
+      cy.get('[data-cy="cardnum-input"]')
+        .clear()
+        .type(`;${userWithAccessCard};{enter}`);
+      cy.url().should("include", "/interlock");
+
+      // Ensure that budget code exist
+      cy.get('[data-cy="toggle-budget"]')
+        .should("exist")
+        .and("have.length.greaterThan", 0);
+
+      //First available budget code.
+      cy.get('[data-cy="toggle-budget"]').first().click();
+      // ensure start is not disabled
+      cy.get('[data-cy="start-button"]').should("not.be.disabled");
+
+      //click start
+      cy.get('[data-cy="start-button"]').click();
+
+      //verify redirection to timer, aka machine turned on 
+      cy.url().should("include", "/timer");
+    });
+  });
+
+  context("User without machine access", () => {
+    it("displays no budget codes and disables the Start button", () => {
+      //Log in as the user as user with no access to LATHE 5000
+      cy.get('[data-cy="cardnum-input"]')
+        .clear()
+        .type(`;${userWithoutAccessCard};{enter}`);
+
+      //redirect to the interlock
+      cy.url().should("include", "/interlock");
+      
+      // Verify that no budget code toggles are rendered.
+      cy.get('[data-cy="toggle-budget"]').should("not.exist");
+
+      // The Start button should be disabled.
+      cy.get('[data-cy="start-button"]').should("be.disabled");
+    });
+  });
+});
