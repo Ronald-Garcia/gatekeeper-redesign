5c79674e06aa3533be0652de7ccdf9d6519e7c1d
ckolisko <koliskocameron@gmail.com>
2025-03-30 15:45:48 -0400

    fixed the problem with financial statements dialog display by copy pasting over that john

--- web/src/components/financialStatements/send-financial-statements-dialog.tsx
+++ web/src/components/financialStatements/send-financial-statements-dialog.tsx
@@ -3,71 +3,32 @@ import { Button } from "../ui/button";
 import { DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog"
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { openPage } from "@nanostores/router";
 import { $router } from "@/data/router";
-import DatePickerWithRange from "./datepicker";
-import { resetDateRange, $date_range } from "@/data/store";
-import { useStore } from "@nanostores/react";
 
 
 const SendFinancialStatementsDialog = () => {
 
+
     const [email, setEmail] = useState("");
-    const [errors, setErrors] = useState({
-        email: false,
-        date: false
-    });
 
-    const dateRange = useStore($date_range);
-
-    const validateInputs = () => {
-        const newErrors = {
-            email: !email,
-            date: !dateRange
-        };
-        setErrors(newErrors);
-        return !Object.values(newErrors).some(error => error);
-    };
-
-    const validateDateRange = () => {
-
-        const newErrors = {
-            email: false,
-            date: !dateRange || (!dateRange.from || !dateRange.to)
-        }
-
-        setErrors(newErrors);
-
-        if (dateRange?.from && dateRange?.to) {
-            return dateRange.from <= dateRange.to;
-        }
-        return false;
+    const handleSendEmail = () => {
+        sendFinancialStatementEmail(email);
     }
 
-    const handleSendEmail = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if (validateInputs()) {
-            sendFinancialStatementEmail(email);
-        } else {
-            e.preventDefault();
-        }
-    }
-    const handleGoToStatementsPage = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if (validateDateRange()) {
-            openPage($router, "financial_statements");
-        } else {
-            e.preventDefault();
-        }
-    }
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
-        setErrors(prev => ({...prev, email: false}));
-    }
+    useEffect(() => {
+        setEmail("rgarci47@jhu.edu");
+    }, []);
+
+
 
     const { sendFinancialStatementEmail } = useMutationEmails();
 
     return (
         <>
+
+        <DialogContent>
             <DialogHeader>
                 <DialogTitle data-cy="financial-statements-title">
                     Send Financial Statements                
@@ -75,43 +36,28 @@ const SendFinancialStatementsDialog = () => {
                 <DialogDescription>
                     Please input the email to send the statements to.    
                 </DialogDescription>    
-                </DialogHeader>
 
-                <div className="flex flex-col space-y-3 p-[20px]">
-                    
-                    <div className="flex flex-row space-x-3">
-                        <Label className="self-center">
-                            Email
-                        </Label>
-                        <Input
-                            className={`w-full ${errors.email ? "border-red-500" : ""}`}
-                            placeholder="Enter email"
-                            data-cy="financial-statements-email"
-                            onChange={handleEmailChange}
-                            value={email}
-                        >
-                        </Input>
-                    </div>
 
-                    <div className="flex flex-row justify-between">
-                        <Label className="self-center">
-                            Date Range
-                        </Label>
-                        <div className={`${errors.date ? "border border-red-500 rounded-md" : ""}`}>
-                            <DatePickerWithRange/>
-                        </div>
-                        <Button variant={"link"} onClick={() => {
-                            resetDateRange();
-                            setErrors(prev => ({...prev, date: false}));
-                        }}>
-                            Reset Date
-                        </Button>
-                    </div>
+                <div className="flex space-x-1">
+                <Label className="self-center">
+                    Email
+                </Label>
+                <Input
+                    defaultValue={"rgarci47@jhu.edu"}
+                    disabled
+                    data-cy="financial-statements-email"
+                    onChange={e => setEmail(e.target.value)}
+                >
+                </Input>
+
                 </div>
 
+                <p className="text-sm italic">Right now, waiting on Resend to allow for other domains. Can only send to account owner</p>
+
                 <DialogFooter>
+
                     <DialogClose asChild>
-                        <Button data-cy = "view-financial-statements" variant={"secondary"} onClick={handleGoToStatementsPage}>
+                        <Button data-cy = "view-financial-statements" variant={"secondary"} onClick={() => openPage($router, "financial_statements")}>
                             ...Or see them here!
                         </Button>
                     </DialogClose>
@@ -126,9 +72,15 @@ const SendFinancialStatementsDialog = () => {
                             Send statements
                         </Button>
                     </DialogClose>
+
                 </DialogFooter>
+            </DialogHeader>
+            </DialogContent>
         </>
+        
     )
+
+
 }
 
 export default SendFinancialStatementsDialog;
