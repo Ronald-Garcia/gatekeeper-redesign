4720b88fbfb3e3e55f09cd7ffaa2f1ce0f5ef5e8
A-Archer <adrian.archer2004@hotmail.com>
2025-04-11 15:30:19 -0400

    Overhauled baseline for interlock and in-progress tsx in order to directly go to QR code as I implement new form method

--- web/src/components/pages/in-progress.tsx
+++ web/src/components/pages/in-progress.tsx
@@ -7,100 +7,86 @@ import useMutationStatements from "@/hooks/use-mutation-financial-statements";
 import { redirectPage } from "@nanostores/router";
 import { $router } from "@/data/router";
 import { $currentUser, $currentMachine } from "@/data/store";
-import ConfirmReportModal from "@/components/modals/ConfirmReportModal"; 
 import useMutationMachineIssue from "@/hooks/use-mutation-machineIssue";
-import ReportFormModal from "@/components/modals/ReportFormmodal";
-
+import ReportFormModal from "@/components/modals/ReportFormmodal"; 
 
 const InProgress = () => {
+  const curUser = useStore($currentUser);
+  const curMachine = useStore($currentMachine);
+  const { reportIssue } = useMutationMachineIssue();
+  const { curBudget, createStatement } = useMutationStatements();
 
-    const curUser = useStore($currentUser);
-    const curMachine = useStore($currentMachine);
-    const { reportIssue } = useMutationMachineIssue();
+  const [isFormModalOpen, setIsFormModalOpen] = useState(false);
+  const [time, setTime] = useState<number>(0);
 
-    const { curBudget, createStatement } = useMutationStatements();
-    
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isFormModalOpen, setIsFormModalOpen] = useState(false);
-
-
-    const [time, setTime] = useState<number>(0);
-
-    useEffect(() => {
-        const interval = setInterval(()=> {
-            setTime(time => time + 1);
-        }, 1000)
-        return () => {
-            clearInterval(interval)
-        };
-    }, []);
-
-    const handleReportIssue = () => {
-        setIsModalOpen(true);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((time) => time + 1);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
     };
+  }, []);
 
-    const handleConfirmReport = async () => {
-        setIsModalOpen(false);
-        
-        const result = await reportIssue(curUser.id, curMachine.id); 
-    
-        if (result) {
-            console.log("Reported issue:", result);
-            setIsFormModalOpen(true); // open the QR modal after reporting
-        }
-    };
-    
+  const handleReportIssue = () => {
+    reportIssue(curUser.id, curMachine.id).then((res) => {
+      if (res) {
+        console.log("Reported issue:", res);
+        setIsFormModalOpen(true);
+      }
+    });
+  };
 
-    
-    const onSubmit = async () => {
-        await createStatement(time);
-        redirectPage($router, "interlockLogin")
-    }
-    return (
-        <>
-            <Card>
-                <CardHeader>
-                        <CardTitle>
-                            Time spent
-                        </CardTitle>
-                        <CardDescription>
-                            Below is the amount of time that will be billed to {curBudget.name}.
-                        </CardDescription>
-                    </CardHeader>
-                <CardContent>
-                    <div data-cy="timer" 
-                        
-                        className="flex justify-center font-bold text-5xl">
-                        <Timer time={time}></Timer>
-                    </div>
-                    </CardContent>
-                    <CardFooter className="relative flex justify-end items-center w-full" data-cy="submit">
-                        <Button className="bg-yellow-400 text-black px-4 py-2 ml-4" variant="ghost" onClick={(e) => { 
-                            e.stopPropagation();  // Prevent the click from bubbling up to the CardFooter's onClick
-                            handleReportIssue(); 
-                        }}> 
-                            Report Issue 
-                        </Button>
-                        <Button className="absolute left-1/2 transform -translate-x-1/2" onClick={onSubmit}>
-                            Tap when finished!
-                        </Button>
-                    </CardFooter>
-            </Card>
+  const onSubmit = async () => {
+    await createStatement(time);
+    redirectPage($router, "interlockLogin");
+  };
 
-            {/* Report Issue Confirmation Modal */}
-            <ConfirmReportModal
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-                onConfirm={handleConfirmReport}
-            />
-            <ReportFormModal
-                isOpen={isFormModalOpen}
-                onClose={() => setIsFormModalOpen(false)}
-            />
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Time spent</CardTitle>
+          <CardDescription>
+            Below is the amount of time that will be billed to {curBudget.name}.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div
+            data-cy="timer"
+            className="flex justify-center font-bold text-5xl"
+          >
+            <Timer time={time} />
+          </div>
+        </CardContent>
+        <CardFooter className="relative flex justify-end items-center w-full" data-cy="submit">
+          <Button
+            className="bg-yellow-400 text-black px-4 py-2 ml-4"
+            variant="ghost"
+            onClick={(e) => {
+              e.stopPropagation();
+              handleReportIssue();
+            }}
+          >
+            Report Issue
+          </Button>
+          <Button
+            className="absolute left-1/2 transform -translate-x-1/2"
+            onClick={onSubmit}
+          >
+            Tap when finished!
+          </Button>
+        </CardFooter>
+      </Card>
 
-        </>
-    );
-}
+      <ReportFormModal
+        isOpen={isFormModalOpen}
+        onClose={() => setIsFormModalOpen(false)}
+      />
+    </>
+  );
+};
 
 export default InProgress;
+
 
