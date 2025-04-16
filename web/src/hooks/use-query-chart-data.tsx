9179646673f1f85b4a5bc481b53c011c099584c5
Ronald Garcia <rgarcia0303757@gmail.com>
2025-04-16 15:26:15 -0400

    Added hooks for chart data

---
+++ web/src/hooks/use-query-chart-data.tsx
@@ -0,0 +1,55 @@
+import { getFinancialStatements } from "@/data/api";
+import { $chart_data, $date_range, resetDateRange, setChartData } from "@/data/store";
+import { SortFinancialType } from "@/data/types/sort";
+import { useStore } from "@nanostores/react";
+import { toast } from "./use-toast";
+import { useEffect } from "react";
+
+
+export function useQueryChart() {
+
+    const dateRange = useStore($date_range);
+    const chartData = useStore($chart_data);
+
+    const getChartFinancialStatements = async (
+        sort: SortFinancialType = "type_asc",
+        page: number = 1,
+        limit: number = 10
+      ) => {
+        try {
+
+        
+            const now = new Date();
+
+            const past = new Date();
+            
+            past.setFullYear(now.getFullYear() - 1);
+   
+            const to = dateRange ? dateRange.to : now;
+            const from = dateRange ? dateRange.from : past;
+
+          const {
+            data
+          } = await getFinancialStatements(sort, page, limit, to as Date, from as Date);
+    
+          setChartData(data);
+    
+        } catch (e) {
+          const errorMessage = (e as Error).message;
+          toast({
+            variant: "destructive",
+            title: "❌ Sorry! There was an error fetching financial statements 🙁",
+            description: errorMessage
+          });
+        }
+    }
+
+    useEffect(() => {
+        resetDateRange();
+        getChartFinancialStatements();
+    }, [])
+
+    return { chartData, getChartFinancialStatements }
+}
+
+export default useQueryChart;
