a10bfc354e5a2c442857267130a45a2c821aaf67
Ronald Garcia <rgarcia0303757@gmail.com>
2025-04-16 15:24:32 -0400

    Committed so far

--- web/src/hooks/use-financialStatements-hook.tsx
+++ web/src/hooks/use-financialStatements-hook.tsx
@@ -2,6 +2,7 @@ import { getFinancialStatements } from "@/data/api";
 import {
   $date_range,
   $statements, 
+  setChartData, 
   setFinancialStatements,
   setMetaData,
  } from "@/data/store";
@@ -36,6 +37,29 @@ function useQueryStatements(reload: boolean) {
       });
     }
   };
+
+  const getChartFinancialStatements = async (
+    sort: SortFinancialType = "type_asc",
+    page: number = 1,
+    limit: number = 10
+  ) => {
+    try {
+
+      const {
+        data
+      } = await getFinancialStatements(sort, page, limit, dateRange!.to as Date, dateRange!.from as Date);
+
+      setChartData(data);
+
+    } catch (e) {
+      const errorMessage = (e as Error).message;
+      toast({
+        variant: "destructive",
+        title: "❌ Sorry! There was an error fetching financial statements 🙁",
+        description: errorMessage
+      });
+    }
+  }
 
   useEffect(() => {
     if (reload) {
