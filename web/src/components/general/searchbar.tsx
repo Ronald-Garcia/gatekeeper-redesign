775e442a8cefe8fce32bf78614eee383b822f4ec
cwang225 <cwang225@jhu.edu>
2025-04-20 11:20:04 -0400

    fixed searchbar UI

--- web/src/components/general/searchbar.tsx
+++ web/src/components/general/searchbar.tsx
@@ -58,6 +58,8 @@ const Searchbar = () => {
                         ? "Search users..."
                         : router?.route === "budgetCodes"
                         ? "Search budget codes..."
+                        : router?.route === "userDashboardMachinesStatus"
+                        ? "Search machines..."
                         : "Search..."}
                     className="w-full h-12 pl-10 pr-4 text-base transition-all duration-200 bg-white border border-gray-200 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500/50 focus:border-blue-500"
                     onKeyDown={handleKeydown}
