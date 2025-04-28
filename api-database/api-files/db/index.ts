c70105ed9d116e9d115c3bf2d83a389017ce6422
omunoz03 <omunoz4@jh.edu>
2025-04-27 22:36:59 -0400

    fixed workflows with env and a atest

--- api-database/api-files/db/index.ts
+++ api-database/api-files/db/index.ts
@@ -2,12 +2,10 @@ import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import dotenv from "dotenv";
-dotenv.config({ path: "./api-database/.env" }); 
-
-config({ path: '.env' }); // or .env.local
+dotenv.config(); 
 
 // the connection string of the postgresql database
-const connectionString = "postgresql://postgres.jiykovdprkyxkstphgwc:mockinterlockdb@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
+const connectionString = process.env.TEST_DB_URL || process.env.PROD_DB_URL!
 if (!connectionString) {
     console.log(connectionString);
     throw Error("null connection string");
