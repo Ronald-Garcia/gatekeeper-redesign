111dfb38ec89f13753558b1f71e16b745688b4f6
ckolisko <koliskocameron@gmail.com>
2025-04-28 11:29:18 -0400

    changed env structure, make sure you make env in root

--- api-database/api-files/db/index.ts
+++ api-database/api-files/db/index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config(); 
 
 // the connection string of the postgresql database
-const connectionString = "postgresql://postgres.jiykovdprkyxkstphgwc:mockinterlockdb@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
+const connectionString =  process.env.TEST_DB_URL || process.env.PROD_DB_URL!
 if (!connectionString) {
     console.log(connectionString);
     throw Error("null connection string");
